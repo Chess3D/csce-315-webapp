7aba7d6898e93b882c51e4f52f0349b803e2d7a6
cjnewman28 <cjnewman28@gmail.com>
2021-04-10 13:12:01 -0400

    Added temporary home page and some user signup functionality

--- GameRing/user/views.py
+++ GameRing/user/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def login(request):
     return render(request, 'user/login.html')
 
+def home(request):
+    return render(request, 'user/home.html')
+
 def signup(request):
-    return render(request, 'user/SignUp.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            message.success(request, f'Account created for user {username}')
+            return redirect('user/login.html')
+    else:
+        form = UserCreationForm()
+    return render(request, 'user/SignUp.html', {'form': form})
 
 
