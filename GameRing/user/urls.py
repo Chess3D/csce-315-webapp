35c2520b9db143723ee5f12be31cf657de74b9e4
cjnewman28 <cjnewman28@gmail.com>
2021-04-10 22:16:32 -0400

    Added Sign Up feature

--- GameRing/user/urls.py
+++ GameRing/user/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.conf.urls import url
+from django.contrib import admin
 
 urlpatterns = [
     path('login/', views.login, name='login'),
-    path('signup/', views.signup, name= 'signup'),
+    url(r'^signup/$', views.signup, name='signup'),
+    # path('signup/', views.signup, name= 'signup'),
     path('', views.home, name='home'),
 
 ]
--- GameRing/user/views.py
+++ GameRing/user/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import SignUpForm
+from django.contrib.auth import login, authenticate
 
 def login(request):
     return render(request, 'user/login.html')
@@ -11,14 +13,16 @@ def home(request):
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            message.success(request, f'Account created for user {username}')
-            return redirect('user/login.html')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('user/home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'user/SignUp.html', {'form': form})
 
 
