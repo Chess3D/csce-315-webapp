1c1a5c736bc73c6a7dd6e740b8d38406e973d3ec
T-Jake-Rod <t.jakerod@gmail.com>
2021-04-11 16:51:25 -0500

    added services.py for tournament API

---
+++ GameRing/tournaments/services.py
@@ -0,0 +1,31 @@
+import os
+import requests
+import json
+
+#tournament API GET
+def get_tournaments():
+    url = 'https://api.challonge.com/v1/tournaments/'
+    r = requests.get(url, headers={'api_key' : '7bG0Ob124vNhDgKA0oktDfuRgiC5jKziYPTF3NUp'})
+    tournaments = r.json()
+    tournament_list = []
+    for i  in range(len(tournaments['tournament'])):
+        tournament_list.append(tournaments['tournament'][i])
+    return tournament_list
+
+#tournament API POST
+def create_tournament(parameters):
+    url = 'https://api.challonge.com/v1/tournaments.json'
+    data = {'api_key' : '7bG0Ob124vNhDgKA0oktDfuRgiC5jKziYPTF3NUp',
+            'name' : parameters['name'],
+            'game_name' : parameters['tournamentGameName'],
+            'tournament_type' : parameters['tournamentType'],
+            'grand_finals_modifer' : parameters['tournamentGrandFinalsMod'],
+            'signup_cap' : parameters['tournamentSignUpCap'],
+            'start_at' : parameters['tournamentStartDate'],
+            'hold_third_place_match' : parameters['tournamentThirdPlaceMatch'],
+            'show_rounds' : parameters['tournamentShowRounds'],
+            'private' : parameters['tournamentPrivate'],
+            'description' : parameters['tournamentDescription']
+            }
+    r = requests.post(url, data)
+    return r.json()
