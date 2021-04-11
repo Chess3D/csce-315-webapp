7c029c6ee7740b767cdf5359f7a6486f89a7e34c
Chess3D <bennettacarley@gmail.com>
2021-04-11 18:08:02 -0500

    Worked login, sign up, and tournaments UI

--- GameRing/user/forms.py
+++ GameRing/user/forms.py
@@ -4,19 +4,13 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    # email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'email', 'password1', 'password2')
 
         widgets = {
-           'username': forms.TextInput(attrs={'class': 'form-control'}),
-           'first_name': forms.TextInput(attrs={'class': 'form-control'}),
-           'last_name': forms.TextInput(attrs={'class': 'form-control'}),
-           #'email': forms.EmailField(attrs={'class': 'form-control'}),
-           'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
-           'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+           'username': forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}),
+           'email': forms.EmailInput(attrs={'class': 'form-control'}),
         }
